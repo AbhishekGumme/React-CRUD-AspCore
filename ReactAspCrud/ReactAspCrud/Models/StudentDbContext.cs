bf9516cee9512ab254d331cd560673f73c3aa99c
aabhishek <aabhishekgumme9011@gmail.com>
2024-02-05 15:36:17 +0530

    crud commit

---
+++ ReactAspCrud/ReactAspCrud/Models/StudentDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ReactAspCrud.Models
+{
+    public class StudentDbContext :DbContext
+    {
+        public StudentDbContext(DbContextOptions<StudentDbContext> options) : base(options)
+        {
+        }
+        public DbSet<Student> Student { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=DESKTOP-TAFC468\\SQLEXPRESS; Initial Catalog=ReactAsp; User Id=DESKTOP-TAC468; password=Abhishek@123; TrustServerCertificate= True");
+        }
+    }
+}
